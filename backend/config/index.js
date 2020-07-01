3083bdf993429c8594f5bfda4652ab4eec8a8b7d
this-is-jassa <jassa.developer@gmail.com>
2020-07-01 17:59:39 -0400

    Mongo connected

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,7 +3,7 @@ const mongo = require('mongoose');
 module.exports = {
     connectMongo: () => {
 
-        mongo.connect('mongodb+srv://jashan:jashan123@cluster0.huicb.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true })
+        mongo.connect('mongodb+srv://jashan:jashan123@cluster0.huicb.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
             .then(_ => {
                 console.log('Mongo Connected');
             }, err => {
