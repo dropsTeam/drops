b82cab25dfd8ea04602a606df22b689b5fe71ef9
NavjotSingh01 <62318415+NavjotSingh01@users.noreply.github.com>
2020-07-13 01:05:55 -0400

    Update product.mid.js

--- backend/src/controllers/product.mid.js
+++ backend/src/controllers/product.mid.js
@@ -6,11 +6,11 @@ const productModel = require('../models/products.model');
 const basicProductInfo = async (req, res, next) => {
 
     try {
-        const { productId } = (req.method === 'POST')? req.body: req.params;
+        const { productId } = (req.method === 'POST') ? req.body : req.params;
 
         const product = await productModel.findOne({ _id: productId }).select('title media varients dropdown totalReview price seller').lean();
-        
-        if(!!!product) {throw 'No Product Found with the given id!'};
+
+        if (!!!product) { throw 'No Product Found with the given id!' };
 
         req.app.locals.productInfo = product;
 
@@ -18,7 +18,7 @@ const basicProductInfo = async (req, res, next) => {
 
     } catch (err) {
         console.log(err);
-        res.status(500).send({ msg: 'Error Occured fetching the product info', err});
+        res.status(500).send({ msg: 'Error Occured fetching the product info', err });
     }
 }
 
@@ -40,4 +40,32 @@ const getbasicProductInfo = async (req, res, next) => {
 }
 
 
-module.exports = { basicProductInfo, getbasicProductInfo };
+const postProduct = async (req, res, next) => {
+    try {
+        const {user} = req.app.locals;
+        const {title, discription, summary, tags, details, media, dropdown, varients, price, category} = req.body;
+        
+        if(tags.length > 20 || details.length > 20 ||  media.length != 5 || dropdown.options.length > 10 || varients.length > 10  ) throw 'Validation Error.';
+
+
+        const payload = {
+            seller: user._id,
+            title,
+            discription,
+            summary,
+            tags,
+            details, media, dropdown, varients, price, category
+        };
+
+        const product = await productModel.create(payload);
+
+        res.status(200).send(product);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({ msg: 'Error Occured Posting the product info' });
+    }
+}
+
+
+module.exports = { basicProductInfo, getbasicProductInfo, postProduct };
