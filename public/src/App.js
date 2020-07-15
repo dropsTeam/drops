624b1e98b86c7c17ad27a8d729618960e8aac371
this-is-jassa <jassa.developer@gmail.com>
2020-07-15 00:19:16 -0400

    Product view html template added

--- public/src/App.js
+++ public/src/App.js
@@ -28,35 +28,29 @@ class App extends React.Component {
 
         <nav>
           <NavBar />
-          This is a Nav Bar only visible for large devices
-          <GoogleBtn visible={true} />
-          
-          
-          
-          
-          {/* {!authorised && <div className='g-signin2' data-onsuccess={onSignIn}> </div>} */}
+
+          {/* <GoogleBtn visible={true} /> */}
         </nav>
 
-        <div>This is sidebar only visible for mobile</div>
 
         {/* Please replace render with component down below */}
-        <Router>
+        {/* <Router>
             <Switch>
 
               <Route path="/" exact render={(props) => <h1>This is Home page</h1>} />
               <Route path="/s/cart" exact render={(props) => <h1>This is Home's cart </h1>} />
 
-              {/* Use PrivateRoute to protect a route */}
+
               <PrivateRoute access={this.props.authorised} path='/s/orders' exact component={(props) => <h1>This is Orders page</h1>} />
               <PrivateRoute access={this.props.authorised} path='/s/account' exact component={(props) => <h1>This is Account page</h1>} />
               <PrivateRoute access={this.isSeller} path='/s/seller' exact component={(props) => <h1>This is Seller page if it exist</h1>} />
 
               <Route path="/:id" exact render={(props) => <h1>This is Customer's Store </h1>} />
 
-              {/* and so on .........  */}
+
 
             </Switch>
-        </Router>
+        </Router> */}
       </React.Fragment>
     );
   }
