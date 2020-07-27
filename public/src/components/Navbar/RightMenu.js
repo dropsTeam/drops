cef6a1c2596f325d63dc462e09f74f5b237e469d
kiran <kirandeepstar123@gmail.com>
2020-07-27 17:15:29 -0400

    added the  login  functionality in the navbar
    added the login functionality in the navbar

--- public/src/components/Navbar/RightMenu.js
+++ public/src/components/Navbar/RightMenu.js
@@ -4,9 +4,61 @@ import { ShoppingCartOutlined } from '@ant-design/icons';
 import DropDown from '../Navbar/dropdown';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import GoogleBtn from '../../container/GoogleBtn/GoogleBtn.js';
 
+
+
+import { Modal } from 'antd';
+
+class App extends React.Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <p type="primary" onClick={this.showModal}>
+          Login
+        </p>
+        <Modal
+          
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={null}
+        >
+          <p>Login here to start your business as a seller</p>
+          <GoogleBtn visible={true} />
+        </Modal>
+      </>
+    );
+  }
+}
+
+
+
+
+// login button and cart button
 class RightMenu extends Component {
-
 
   render() {
 
@@ -15,16 +67,16 @@ class RightMenu extends Component {
         {
           (!this.props.isAuthorised) && (
             <Button className="logbtn">
-              Login
+              <App />
             </Button>
 
           )
         }
 
         <DropDown />
-        <Menu.Item style={{ position: "relative", right: 25 }} key="1">
-          <NavLink to='/cart' style={{ color: 'white' }}>
-            <Badge count={this.props.cartItems.length} style={{ position: "absolute", top: 8, right: 10, fontSize: 12, }}><ShoppingCartOutlined style={{ fontSize: 28, top: 7, position: "relative" }} /></Badge>Cart
+        <Menu.Item style={{ position: "relative", right: 25 ,top: '-4px' }} key="1">
+          <NavLink to='/cart' style={{ color: 'white' ,textDecoration : "none"}}>
+            <Badge count={this.props.cartItems.length} style={{ position: "absolute", top: '7px', right: 10, fontSize: 12, }}><ShoppingCartOutlined style={{ fontSize: 28, top: 7, position: "relative" }} /></Badge>
           </NavLink>
         </Menu.Item>
       </Menu>
