207fb420373836cd1088781fef14aa8f32545ee6
kiran <kirandeepstar123@gmail.com>
2020-07-29 16:26:31 -0400

    added icon to the search field.
    added icon to the search field.

--- public/src/components/Navbar/LeftMenu.js
+++ public/src/components/Navbar/LeftMenu.js
@@ -1,81 +1,144 @@
 import React, { Component } from 'react';
+// import Countries from "./Country"
 import {mainHttp as axios} from "../../Axios/Axios.js";
 import { Input, Dropdown} from 'antd';
+import ButtonGroup from 'antd/lib/button/button-group';
 const { Search } = Input;
 
+const Countries =[
+  "T-Shirts",
+  "Lipsticks",
+  "Home Decor",
+  "Electronics",
+  "Men'wear",
+  "Kids Clothes"
+]
 
-class SearchedList extends Component{
+class AutoCompletedText extends React.Component{
+ constructor(props){
+   super(props);
+   this.state={
+     suggestions:Countries,
+     text:'',
+     focus:false
+   }
+   this.onTextChange = this.onTextChange.bind(this.onTextChange)
+   this.handleFocus = this.handleFocus.bind(this);
+   this.handleBlur = this.handleBlur.bind(this);
+ }
+
+
+ onTextChange=(e) => {
+  let suggestions = Countries;
+  let value = e.target.value;
+  if(value.length > 0){
+    let patt = new RegExp(`^${value}`,'i')
+    suggestions = Countries.filter(v => patt.test(v));
+  }
+
+   this.setState({
+     text:e.target.value,
+     suggestions : suggestions
+   })
+ }
+ 
+
+ selectedItem = (value) =>{
+  this.setState({
+    text:value,
+    suggestions:Countries,
+    focus:true
+  })
+  this.setState({focus:false})
+}
+
+
+handleFocus(e) {
+  e.preventDefault();
+  console.log('The link was clicked.');
+  axios.get("/user/searchHistory")
+    .then(res=>{
+      console.log(res);
+   })
+  this.setState({focus:true}) 
+}
+
+
+
+handleBlur(e) {
+  e.preventDefault();
+  this.renderSuggestions()
+  setTimeout(()=>this.setState({focus:false}) ,200) 
+}
+
+ renderSuggestions = () => {
+   if(this.state.suggestions.length == 0){
+     return null;
+   }
+   else{
+     return (
+       <ul className={this.state.focus ? "show" : "hide"}>
+         {
+           this.state.suggestions.map((item,index)=>
+          //  there should be no brackets here*****
+           <li key={index}  onClick={() => this.selectedItem(item)}>
+              <span>
+                <img className="recentSearch__image" src="https://rukminim1.flixcart.com/www/100/100/promos/19/07/2018/321e89f8-9ffa-47a7-a9d4-731da9bde6c4.png?q=90"></img>
+              </span> 
+              {item}
+            </li>
+           )
+         }
+       </ul>
+     )
+   }
+ }
+
+
+
+
+
+render(){
+  const {suggestions,text} = this.state;
+   return(
+      <>
+        <Search
+          enterButton
+          size="medium"
+          value={text}
+          onSearch={value => console.log(value)}
+          onChange={this.onTextChange} 
+          onFocus={this.handleFocus}  
+          onBlur={this.handleBlur} 
+        />
+      {this.renderSuggestions()}
+      </>
+   )
+ }
+}
+
+
+class LeftMenu extends Component{
 
   constructor(props) {
     super(props);
   }
 
-  render(){
 
+  render(){
     let focus = this.props.focus;
     return (
-      <div className={`searched__list`}>
-        <ul className={focus ? "" : "show"} >
-        <li>cat</li>
-        <li>dog</li>
-        </ul>
-      </div>
+       <div className={`searched__list`}>
+          <AutoCompletedText />
+          {/* <Search
+          enterButton
+          size="medium" */}
+      
+       </div>
     )
   }
 }
 
 
-
-
-class LeftMenu extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state={
-       focus : false
-    }
-
-    this.handleFocus = this.handleFocus.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-  }
- 
-
-
-  handleFocus(e) {
-    e.preventDefault();
-    console.log('The link was clicked.');
-    axios.get("/user/searchHistory")
-      .then(res=>{
-        console.log(res);
-     })
-    this.setState({focus:true}) 
-  }
-
-
-  
-  handleBlur(e) {
-    e.preventDefault();
-    this.setState({focus:false}) 
-  }
-
-
-  render() {
-
-    return (
-      
-     <>
-      <Search className="searchbox"
-          placeholder="Search for products, brands and more"
-          onSearch={value => console.log(value)} 
-          onFocus={this.handleFocus}  
-          onBlur={this.handleBlur} 
-       />  
-
-       <SearchedList focus={this.state.focus}/>
-
-			</>
-    );
-  }
-}
 
 export default LeftMenu;
