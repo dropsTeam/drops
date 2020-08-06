0671a36d7ee67b86cf2c5c5527593cae8c40ade7
Jashanpreet Singh <51172269+this-is-jassa@users.noreply.github.com>
2020-08-06 12:39:06 -0400

    logout issue resolved

--- public/src/container/sellerPortal/sellerPortal.js
+++ public/src/container/sellerPortal/sellerPortal.js
@@ -11,7 +11,7 @@ import AnswerQuestionModal from './Modals/answerQuestions';
 
 import OrderViewModal from './Modals/orderViewModal';
 import { connect } from 'react-redux';
-import { editSellerAccount } from '../../Redux/Actions/AuthActions';
+import { editSellerAccount, logout } from '../../Redux/Actions/AuthActions';
 
 import { mainHttp as axios } from "../../Axios/Axios";
 
@@ -96,16 +96,16 @@ class SellerPortal extends React.Component {
                     </a>
                 </Menu.Item>
                 <Menu.Item>
-                    <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                    <a target="_blank" rel="noopener noreferrer" onClick={this.props.$logout}>
                         Logout
-                </a>
+                    </a>
                 </Menu.Item>
             </Menu>
         );
 
-        if(this.props.isAuthorised) {
+        if (this.props.isAuthorised) {
             return (
-    
+
                 <div>
                     <AddProduct isVisible={this.state.view.AddProduct} $toggleModal={(modalName, visible) => this.toggleModal(modalName, visible)} />
                     <EditSellerProfile isVisible={this.state.view.EditSellerProfile} $toggleModal={(modalName, visible) => this.toggleModal(modalName, visible)} />
@@ -120,21 +120,21 @@ class SellerPortal extends React.Component {
                             </Dropdown>,
                             <Button key="3" onClick={() => this.toggleModal('orderView', true)}>Orders</Button>,
                             <Button key="2" onClick={() => this.toggleModal('AnswerQuestions', true)} >Pending Questions</Button>,
-    
+
                         ]}
                     />
-    
+
                     <div style={{ color: 'white', margin: '0px', background: '#0099ff' }}>
                         <p className='m-0 h2 p-4'>
                             {(this.props.user.seller.profileImg === 'Default' || this.props.user.seller.profileImg.length === 0) ? <Avatar style={{ marginRight: '30px' }} size="large" icon={<UserOutlined />} /> : <Avatar style={{ marginRight: '30px' }} size="large" src={this.props.user.seller.profileImg} />}
                             Welcome Back {this.props.user.seller.name}
                         </p>
                         <pre className='m-0 p-4 h6' style={{ color: 'white', paddingLeft: '30px', whiteSpace: 'pre-wrap' }}>{this.props.user.seller.bio}</pre>
-    
+
                     </div>
                     <svg style={{ margin: '0px' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,128L48,128C96,128,192,128,288,138.7C384,149,480,171,576,170.7C672,171,768,149,864,160C960,171,1056,213,1152,197.3C1248,181,1344,107,1392,69.3L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
-    
-    
+
+
                     <div className="row" style={{ padding: '0 20px' }}>
                         <div className="col-sm-12 col-md-4 mt-2 ">
                             <div className="card" style={{ backgroundColor: '#FEC009', color: 'white' }}>
@@ -147,7 +147,7 @@ class SellerPortal extends React.Component {
                         <div className="col-sm-12 col-md-4 mt-2 ">
                             <div className="card" style={{ backgroundColor: '#DD6777', color: 'white' }}>
                                 <div className="card-body">
-    
+
                                     <p className="card-text">Number Of Orders</p>
                                     <h1 className="card-title" style={{ color: 'white' }}>{this.state.stats.totalOrders}</h1>
                                 </div>
@@ -161,37 +161,37 @@ class SellerPortal extends React.Component {
                                 </div>
                             </div>
                         </div>
-    
+
                     </div>
                     <hr />
-    
+
                     <div>
                         <p className='h2' style={{ padding: '30px 0 0 20px' }}>Your Products</p>
                         <div className={styles.results__block} >
                             {(this.state.products.length === 0) ? <img src='/public/public/logo192.png' /> : <ResultsBlock numbers={this.state.products} />}
                         </div>
                     </div>
-    
-    
+
+
                     <AnswerQuestionModal $toggleModal={(a, b) => this.toggleModal(a, b)} isVisible={this.state.view.AnswerQuestions} />
                     <OrderViewModal $toggleModal={(a, b) => this.toggleModal('orderView', b)} isVisible={this.state.view.orderView} />
                     <EditSellerProfile
                         name={this.props.user.seller.name}
                         bio={this.props.user.seller.bio}
                         profileImg={this.props.user.seller.profileImg}
-                        
+
                         onSave={(data) => this.props.$editSellerProfile(data)}
                         isVisible={this.state.view.EditSellerProfile}
                         $toggleModal={(a) => this.toggleModal('EditSellerProfile', a)} />
-    
+
                 </div>
             )
         } else {
             this.props.history.goBack();
-            return(
+            return (
                 <h1></h1>
             )
-            
+
         }
 
 
@@ -209,7 +209,8 @@ const mapPropsToState = store => {
 const mapPropsToDispatch = dispatch => {
 
     return {
-        $editSellerProfile: (data) => dispatch(editSellerAccount(data))
+        $editSellerProfile: (data) => dispatch(editSellerAccount(data)),
+        $logout: () => dispatch(logout())
     }
 }
 
