d66803851919f8c6665f58f1bcfcc46465c42bab
Jashanpreet Singh <51172269+this-is-jassa@users.noreply.github.com>
2020-08-09 15:55:14 -0400

    b

--- backend/config/index.js
+++ backend/config/index.js
@@ -4,11 +4,11 @@ const mongo = require('mongoose');
 module.exports = {
 
     auth: {
-        GOOGLE_CLIENT_ID: '1092942175302-0ibod3kvsqd9861k4q88epeaa2q2t587.apps.googleusercontent.com'
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '1092942175302-0ibod3kvsqd9861k4q88epeaa2q2t587.apps.googleusercontent.com'
     },
     connectMongo: () => {
 
-        mongo.connect('mongodb+srv://jashan:jashan123@cluster0.huicb.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+        mongo.connect(process.env.CONNECT_STRING || 'mongodb+srv://jashan:jashan123@cluster0.huicb.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
             .then(_ => {
                 console.log('Mongo Connected');
             }, err => {
